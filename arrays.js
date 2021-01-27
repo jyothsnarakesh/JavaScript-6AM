b95279ca3b67fb9e006651026bade77a810f2416
samba.pendela@gmail.com <samba.pendela@gmail.com>
2021-01-27 06:40:07 +0530

    arrays

--- arrays.js
+++ arrays.js
@@ -152,7 +152,7 @@ console.log(
         - used to remove/add elements at any index of array
 
         - mutable
-*/
+
 let arr = [10,20,30,40,50,60,70,80,90,100];
 console.log( arr );     //[10, 20, 30, 40,  50, 60, 70, 80, 90, 100]
 
@@ -180,6 +180,118 @@ console.log(arr);           //[ 10, 20, 30, 40, 50, 60, 70 ]
 
 arr.splice(6,0,80,90,100);
 console.log(arr);           //[10, 20, 30, 40,  50, 60, 70, 80, 90, 100]
+*/
+
+/*
+        13) findIndex()
+
+            - to find the index of particular element.
+*/
+
+
+/*
+let arr = [10,20,30,40,50];
+console.log(
+    arr.findIndex((element,index)=>{
+        return element === 30;
+    })
+);     //2
+console.log(
+    arr.findIndex((element,index)=>{
+        return element === 300;
+    })
+);     //-1
+
+let arr1 = [{"p_id":111,"p_name":"p_one","p_cost":10000},
+            {"p_id":222,"p_name":"p_two","p_cost":20000},
+            {"p_id":333,"p_name":"p_three","p_cost":30000},
+            {"p_id":444,"p_name":"p_four","p_cost":40000},
+            {"p_id":555,"p_name":"p_five","p_cost":50000}];
+console.log(
+    arr1.findIndex((element,index)=>{
+        return element.p_id === 333
+    })
+);    //2
+
+arr.splice(arr.findIndex((element,index)=>{
+    return element === 40
+}),1);
+console.log(arr);                                       //[ 10, 20, 30, 50 ]
+
+arr1.splice(arr1.findIndex((element,index)=>{
+    return element.p_id === 222
+}),1);
+console.log(arr1);   //[
+                            //{ p_id: 111, p_name: 'p_one', p_cost: 10000 },
+                            //{ p_id: 333, p_name: 'p_three', p_cost: 30000 },
+                            //{ p_id: 444, p_name: 'p_four', p_cost: 40000 },
+                            //{ p_id: 555, p_name: 'p_five', p_cost: 50000 }
+                    //]
+*/
+
+//slice()
+//get the selected items
+
+let arr1 = [10,20,30,40,50,60,70,80,90,100];
+
+console.log( arr1.slice(5,7) );     //[ 60, 70 ]
+console.log( arr1.slice(7,9) );     //[ 80, 90 ]
+console.log( arr1.slice(2,5) );     //[ 30, 40, 50 ]
+console.log( [...arr1.slice(7,10),...arr1.slice(0,3)] );    //[ 80, 90, 100, 10, 20, 30 ]
+
+let arr2 = [0,1,2,3,4,5,6,7,8,9];
+let a1 = arr2.slice(5);
+let a2 = arr2.slice(0,5);
+let a3 = a2.concat(a1);
+console.log(a3);
+console.log([...a2,...a1]);
+
+let arr3 = [100,200,300,400,500,600,700,800,900,10000];
+console.log( arr3.slice(-6,-4) );                   //[ 500, 600 ]
+console.log( arr3.slice(-9,-6) );                   //[ 200, 300, 400 ]
+console.log( arr3.slice(-10,-9) );                        //[ 100 ]
+console.log( arr3.slice(-1) );                      //[ 10000 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
